918456bfce53859e53bbcfeb2b43cf2591cc613e
Mauro <34516696+MauroNr1@users.noreply.github.com>
2023-04-10 00:11:31 +0200

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,35 @@
+Locales['nl'] = {
+    ['application_name'] = 'Burgerschapsapplicatie',
+  
+    ['first_name'] = 'Voornaam',
+    ['first_name_description'] = 'De voornaam van je karakter.',
+  
+    ['last_name'] = 'Achternaam',
+    ['last_name_description'] = 'De achternaam van je karakter.',
+  
+    ['height'] = 'Hoogte',
+    ['height_description'] = 'De hoogte van je karakter in centimeter.',
+  
+    ['dob'] = 'Geboortedatum',
+    ['dob_description'] = 'De geboortedatum van je karakter.',
+  
+    ['gender'] = 'Geslacht',
+    ['gender_description'] = 'Het geslacht van je karakter.',
+    ['gender_male'] = 'Man',
+    ['gender_female'] = 'Vrouw',
+  
+    ['prepared_to_rp'] = 'Ik ben bereid om goed te roleplayen.',
+    ['application_accepted'] = 'Applicatie geaccepteerd, welkom!',
+
+    ['show_active_character'] = 'Toon actief karakter',
+    ['active_character'] = 'Actief karakter: %s',
+    ['error_active_character'] = 'Er was een fout bij het ophalen van je data.',
+    ['delete_character'] = 'Verwijder je huidig karakter.',
+    ['deleted_character'] = 'Karakter verwijdert',
+    ['error_delete_character'] = 'Er was een probleem bij het verwijderen van je karakter.',
+    ['no_identifier'] = '[ESX Identity]\nEr was een fout bij het laden van je karater!\nFoutcode: identifier-missing\n\nZorg ervoor dat je een legale GTA versie hebt en probeer het later nog eens opnieuw.',
+    ['missing_identity'] = '[ESX Identity]\nEr was een fout bij het laden van je karakter!\nFoutcode: identity-missing\n\nHet lijkt erop dat je karakter is verwijdert, of er een probleem is met de database. Probeer eens te rejoinen.',
+    ['deleted_identity'] = 'Karakter verwijdert. Rejoin om een nieuwe te maken.',
+    ['already_registered'] = 'Je hebt al geregistreerd.',
+  }
+  
